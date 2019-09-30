e852698628f5d43eee88f99a527e886d90485a2f
Emmanuel Rocha <emmanuelrocha001@gmail.com>
2019-09-30 08:24:21 -0500

    using bcryptjs instead of bcrypt due to deployment issues

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require( '../models/user' );
-const bcrypt = require('bcrypt');
+var bcrypt = require( 'bcryptjs' );
 const mongoose = require('mongoose');
 
 
