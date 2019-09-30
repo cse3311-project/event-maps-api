480fb606483ea9d3abc5aa0e91c9053d645286fb
Emmanuel Rocha <emmanuelrocha001@gmail.com>
2019-09-30 08:04:01 -0500

    user post request handling implemented

---
+++ api/routes/user.js
@@ -0,0 +1,121 @@
+const express = require('express');
+const router = express.Router();
+const User = require( '../models/user' );
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+
+
+router.post('/signup', (req, res, next) => {
+
+    User.find( {email: req.body.email} )
+      .exec()
+      .then(user => {
+        if (user.length >= 1) {
+          return res.status(409).json({
+            message: 'email is already linked to an existing account'
+          });
+        }
+        else {
+          bcrypt.hash(req.body.password, 10, (err, hash) =>{
+            if (err) {
+              return res.status(500).json({
+                  error: err
+              });
+            } else {
+
+              console.log(hash);
+
+              const user = new User({
+                _id: new mongoose.Types.ObjectId(),
+                email: req.body.email,
+                password: hash
+              });
+
+              //status 201 when creating resource
+              user
+                .save()
+                .then(result => {
+                  console.log(result);
+                  res.status(201).json({
+                    message : 'User created'
+                  });
+                })
+                .catch(err => {
+                  console.log(err);
+                  res.status(500).json({error : err})
+                });
+            }
+          });
+        }
+      })
+});
+
+router.post('/login', exports.user_login = (req, res, next) => {
+    User.find({ email: req.body.email})
+      .exec()
+      .then(user => {
+        if (user.length < 1) {
+          return res.status(401).json({
+            message: 'Auth failed'
+          });
+
+        }else {
+          bcrypt.compare( req.body.password, user[0].password, ( err, result) => {
+            if (err) {
+              return res.status(401).json({
+                message: 'Auth failed'
+              });
+
+            }
+            //result is the truth value of comparison
+            if ( result ) {
+
+            //   const token = jwt.sign(
+            //     {
+            //       email: user[0].email,
+            //       userId: user[0]._id
+            //     },
+            //     process.env.JWT_KEY,
+            //     {
+            //       expiresIn: "1h"
+            //     }
+            //   );
+
+              return res.status(200).json({
+                message: 'Auth successful',
+              });
+
+            } else {
+              return res.status(401).json({
+                message: 'Auth failed'
+              });
+            }
+          });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({error : err})
+      });
+});
+
+
+router.delete('/:userId', (req, res, next) => {
+    const id = req.params.userId;
+    User.remove({ _id: id })
+    .exec()
+    .then(result => {
+      res.status(200).json({
+        message: 'User deleted',
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+//export such that module can be used in other files
+module.exports = router
