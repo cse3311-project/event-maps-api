3fa850e9fecaf6e7bcb3f14298364a24a3e15e64
Emmanuel Rocha <emmanuelrocha001@gmail.com>
2020-03-21 11:55:20 -0500

    cors errors

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use( bodyParser.json()) ;
 app.use((req, res, next) => {
     // deals with cors, gives access to any client
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-requested-Width, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Headers', '*');
     res.header('Access-Control-Allow-Methods', '*');
     res.header('Access-Control-Allow-Origin', '*');
     if (req.method === 'OPTIONS') {
