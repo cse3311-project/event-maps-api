eecdcbdf7ecd7ec27b4dd76683c629f9ba2f929e
Emmanuel Rocha <emmanuelrocha001@gmail.com>
2020-03-21 11:40:35 -0500

    fixed task model

--- api/models/task.js
+++ api/models/task.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 // design object
 const taskSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    taskText: { type: String, required: false},
-    completed: { type: Boolean, required: false, default: true},
+    taskText: { type: String, required: true},
+    completed: { type: Boolean, required: true, default: false},
     datePosted: { type: Date, required: true}
   });
 
