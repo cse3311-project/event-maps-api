12dd99771623839eed5fe8b7ab090e4da2d24972
Emmanuel Rocha <emmanuelrocha001@gmail.com>
2020-03-20 20:27:28 -0500

    changes

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Origin, X-requested-Width, Content-Type, Accept, Authorization');
     if (req.method === 'OPTIONS') {
       res.header('Access-Control-Allow-Origin', 'PUT, POST, PATCH', 'DELETE, GET');
+      res.header('Allow', 'PUT, POST, PATCH', 'DELETE, GET');
       return res.status(200).json({});
     }
 
